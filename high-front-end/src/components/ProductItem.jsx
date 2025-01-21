7b05e82848b7574fa87e3c239856c3c61ebe9643
anyajegorova <anna.jerm@outlook.com>
2025-01-21 20:36:15 +0200

    title and product item

---
+++ high-front-end/src/components/ProductItem.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/shopContext";
+import { Link } from "react-router-dom";
+
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+  return <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+<div className=''></div>
+  </Link>;
+};
+
+export default ProductItem;
