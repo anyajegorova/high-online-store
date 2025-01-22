bb95de105d0d8b71270ab558049b09b151f40bb3
anyajegorova <anna.jerm@outlook.com>
2025-01-22 20:35:19 +0200

    bestseller products and latest collection

--- high-front-end/src/components/BestSeller.jsx
+++ high-front-end/src/components/BestSeller.jsx
@@ -3,30 +3,31 @@ import { ShopContext } from "../context/shopContext";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProducts] = useState([]);
-  useEffect(() => {
-    setLatestProducts(products.slice(0, 10));
-  }, [products]);
+  const [bestSeller, setBestSeller] = useState([]);
 
+  useEffect(() => {
+    const bestProduct = products.filter((product) => product.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
+    console.log(bestProduct);
+  }, []);
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
-      {/*Rendering Products*/}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
             id={item._id}
             image={item.image}
-            name={item.name}
+            name={item.title}
             price={item.price}
           />
         ))}
@@ -35,4 +36,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
