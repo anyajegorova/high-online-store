7af76f1d8f3375bc0247a668d0a0cc847dc7ded0
anyajegorova <anna.jerm@outlook.com>
2025-03-10 13:44:01 +0200

    newsletter box

---
+++ high-front-end/src/components/NewsletterBox.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const NewsletterBox = () => {
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className=" text-center">
+      <p className="text-2xl font-medium text-gray-800">
+        Subscribe now & get 20% off
+      </p>
+      <p className="text-gray-400 mt-3">
+        Get all the latest deals and offers straight to your inbox
+      </p>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3"
+      >
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your email.."
+          required
+        />
+        <button
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4"
+        >
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsletterBox;
