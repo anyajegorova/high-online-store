0c24a4537d391ee998ab356f81753351136a46c2
anyajegorova <anna.jerm@outlook.com>
2025-03-10 14:08:35 +0200

    footer

---
+++ high-front-end/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-10 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="logo" />
+          <p className='w-full md:w-2/3 text-gray-600'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+            ipsum et dui rhoncus auctor.
+          </p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+358-50-854-5368</li>
+                <li>contact@high.com</li>
+            </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025© high.com - All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
